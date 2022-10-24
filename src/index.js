6f78c875abc3204f53aa826b06a38ee07e9fbc06
Miradzyapko <mira.dzyapko@gmail.com>
2022-10-24 22:58:35 +0300

    додає кінцеві файли

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+import { createCountryList, createCountry} from './markup';
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+inputRef: document.querySelector('#search-box'),
+countryInfo: document.querySelector('.country-info'),
+listRef: document.querySelector('.country-list') }
+
+
+refs.inputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+function onSearch(evt) {
+    evt.preventDefault();
+    const countryName = evt.target.value.trim();
+    onClearMarkup();
+    checkSearchValue(countryName)
+   
+}
+function onClearMarkup() {
+    refs.listRef.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+}
+    
+  
+
+//
+function checkValue(resolveData) {
+    if (resolveData.length > 10) {
+        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+    }
+    if (resolveData.length === 0) 
+        {
+         return Notiflix.Notify.failure('Oops, there is no country with that name');
+    
+        
+        }
+
+
+
+    }
+   
+//---
+
+        function renderData(resolve) { 
+
+            if (resolve.length >= 2 && resolve.length <= 10) {
+
+                return createCountryList(resolve);
+            }
+            if (resolve.length === 1) {return createCountry(resolve);}
+        }
+    function checkSearchValue(countryName) {
+        if (countryName !== '') {
+     
+    
+            fetchCountries(countryName)
+            .then(resolve => {
+             checkValue(resolve);
+             renderData(resolve);
+            
+            });
+         }
+
+    }
